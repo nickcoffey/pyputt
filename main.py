106a6a9f0c3f311aa555095eab727ea53b913b31
Nick Coffey <nicholasjcoffey@gmail.com>
2025-02-28 00:22:37 -0600

    feat: added hole, moved ball to beginning on collision

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ CLOCK = pygame.time.Clock()
 RUNNING = True
 DELTA_TIME = 0
 
-PLAYER_POS = pygame.Vector2(SCREEN.get_width() / 2, SCREEN.get_height() / 2)
 BALL_SPEED = 600
 BALL_SIZE = 40
 BALL_RADIUS = BALL_SIZE / 2
+PLAYER_START = pygame.Vector2(BALL_SIZE + 20, SCREEN.get_height() - BALL_SIZE - 20)
+PLAYER_POS = pygame.Vector2(PLAYER_START)
 
 
 def check_bounds() -> None:
@@ -29,48 +30,20 @@ def check_bounds() -> None:
 
 
 def detect_collision(temp_course: Rect, temp_ball: Rect) -> None:
-    ball_left = (temp_ball.x, temp_ball.y - BALL_RADIUS)
-    ball_right = (temp_ball.x + BALL_SIZE, temp_ball.y - BALL_RADIUS)
-    ball_top = (temp_ball.x + BALL_RADIUS, temp_ball.y)
-    ball_bottom = (temp_ball.x + BALL_RADIUS, temp_ball.y + BALL_SIZE)
-    #
-    # if temp_course.collidepoint(ball_left):
-    #     PLAYER_POS.x = -BALL_SPEED * DELTA_TIME
-
-    # print(temp_course.collidelistall([temp_ball]))
-
-    rect_left = (temp_course.x, temp_course.y - BALL_RADIUS)
-    rect_right = (temp_course.x + BALL_SIZE, temp_course.y - BALL_RADIUS)
-    rect_top = (temp_course.x + BALL_RADIUS, temp_course.y)
-    rect_bottom = (temp_course.x + BALL_RADIUS, temp_course.y + BALL_SIZE)
     if temp_course.colliderect(temp_ball):
-        PLAYER_POS.x = LAST_POS.x
-        PLAYER_POS.y = LAST_POS.y
-        # if ball_left[0] < rect_right[0]:
-        #     PLAYER_POS.x = rect_right[0] + BALL_SIZE
-        # elif ball_right[0] > rect_left[0]:
-        #     PLAYER_POS.x = rect_left[0] - BALL_SIZE
-        # elif ball_top[1] > rect_bottom[1]:
-        #     PLAYER_POS.y = rect_bottom[1] + BALL_SIZE
-        # elif ball_bottom[1] < rect_top[1]:
-        #     PLAYER_POS.y = rect_top[1] - BALL_SIZE
-
-
-LAST_POS = pygame.Vector2(PLAYER_POS.x, PLAYER_POS.y)
+        PLAYER_POS.x = PLAYER_START.x
+        PLAYER_POS.y = PLAYER_START.y
 
 
 def move_ball() -> None:
-    LAST_POS.x = PLAYER_POS.x
-    LAST_POS.y = PLAYER_POS.y
-
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_i]:
+    if keys[pygame.K_i] or keys[pygame.K_w]:
         PLAYER_POS.y -= BALL_SPEED * DELTA_TIME
-    if keys[pygame.K_k]:
+    if keys[pygame.K_k] or keys[pygame.K_s]:
         PLAYER_POS.y += BALL_SPEED * DELTA_TIME
-    if keys[pygame.K_j]:
+    if keys[pygame.K_j] or keys[pygame.K_a]:
         PLAYER_POS.x -= BALL_SPEED * DELTA_TIME
-    if keys[pygame.K_l]:
+    if keys[pygame.K_l] or keys[pygame.K_d]:
         PLAYER_POS.x += BALL_SPEED * DELTA_TIME
 
 
@@ -81,6 +54,11 @@ def draw_course() -> Rect:
         pygame.Rect(SCREEN.get_width() / 4, SCREEN.get_height() / 4, 20, 20),
         BALL_SIZE,
     )
+
+
+def draw_hole() -> Rect:
+    hole_pos = pygame.Vector2(SCREEN.get_width() - 120, SCREEN.get_height() - 120)
+    return pygame.draw.circle(SCREEN, "black", hole_pos, 20)
 
 
 while RUNNING:
@@ -95,8 +73,8 @@ while RUNNING:
     course = draw_course()
     ball = pygame.draw.circle(SCREEN, "white", PLAYER_POS, BALL_SIZE)
 
+    draw_hole()
     move_ball()
-    print(f"POS: {PLAYER_POS}, LAST_POS: {LAST_POS}")
     check_bounds()
     detect_collision(course, ball)
 
