af4f01ba07e0761e9bfe7d53e4b1a407e46a9a5c
Nick Coffey <nicholasjcoffey@gmail.com>
2025-02-28 00:22:37 -0600

    feat: added boundary collision detection

--- main.py
+++ main.py
@@ -10,6 +10,32 @@ DELTA_TIME = 0
 
 PLAYER_POS = pygame.Vector2(SCREEN.get_width() / 2, SCREEN.get_height() / 2)
 BALL_SPEED = 600
+BALL_SIZE = 40
+
+
+def check_bounds():
+    if PLAYER_POS.x < BALL_SIZE:
+        PLAYER_POS.x = BALL_SIZE
+    elif PLAYER_POS.x > SCREEN.get_width() - BALL_SIZE:
+        PLAYER_POS.x = SCREEN.get_width() - BALL_SIZE
+
+    if PLAYER_POS.y < BALL_SIZE:
+        PLAYER_POS.y = BALL_SIZE
+    elif PLAYER_POS.y > SCREEN.get_height() - BALL_SIZE:
+        PLAYER_POS.y = SCREEN.get_height() - BALL_SIZE
+
+
+def move_ball():
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_i]:
+        PLAYER_POS.y -= BALL_SPEED * DELTA_TIME
+    if keys[pygame.K_k]:
+        PLAYER_POS.y += BALL_SPEED * DELTA_TIME
+    if keys[pygame.K_j]:
+        PLAYER_POS.x -= BALL_SPEED * DELTA_TIME
+    if keys[pygame.K_l]:
+        PLAYER_POS.x += BALL_SPEED * DELTA_TIME
+
 
 while RUNNING:
     # poll for events
@@ -20,18 +46,10 @@ while RUNNING:
 
     # fill the screen with a color to wipe away anything from last frame
     SCREEN.fill("green")
+    pygame.draw.circle(SCREEN, "white", PLAYER_POS, BALL_SIZE)
 
-    pygame.draw.circle(SCREEN, "white", PLAYER_POS, 40)
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_i]:
-        PLAYER_POS.y -= BALL_SPEED * DELTA_TIME
-    if keys[pygame.K_k]:
-        PLAYER_POS.y += BALL_SPEED * DELTA_TIME
-    if keys[pygame.K_j]:
-        PLAYER_POS.x -= BALL_SPEED * DELTA_TIME
-    if keys[pygame.K_l]:
-        PLAYER_POS.x += BALL_SPEED * DELTA_TIME
+    move_ball()
+    check_bounds()
 
     # flip() the display to put your work on screen
     pygame.display.flip()
