70f654f79ffc86af4d00b8927285fa234768b3a4
Nick Coffey <nicholasjcoffey@gmail.com>
2025-02-28 00:22:37 -0600

    feat: moved course code to its own folder

---
+++ libs/course/course.py
@@ -0,0 +1,39 @@
+import json
+import pygame
+
+from pygame import Rect, Surface, Vector2
+
+
+with open("libs/course/course_grid.json", encoding="utf-8") as f_in:
+    COURSE_GRID = json.load(f_in)
+
+
+def draw_course(ball_size: int, screen: Surface) -> tuple[list[Rect], Rect, Vector2]:
+    ball_start_pos = Vector2(0, 0)
+    box_size = 40
+    hole_size = 20
+    course: list[Rect] = []
+    hole: Rect = pygame.draw.circle(
+        screen, "black", Vector2(hole_size, hole_size), hole_size
+    )
+
+    for y, row in enumerate(COURSE_GRID):
+        for x, value in enumerate(row):
+            if value == 1:
+                course.append(
+                    pygame.draw.rect(
+                        screen,
+                        "brown",
+                        pygame.Rect(x * box_size, y * box_size, box_size, box_size),
+                        box_size,
+                    )
+                )
+            elif value == 2:
+                ball_start_pos = Vector2(
+                    (x * box_size) + (ball_size * 2), y * box_size + ball_size
+                )
+            elif value == 3:
+                hole_pos = Vector2(x * box_size + hole_size, y * box_size + hole_size)
+                hole = pygame.draw.circle(screen, "black", hole_pos, hole_size)
+
+    return course, hole, ball_start_pos
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 # Example file showing a circle moving on screen
 import pygame
-import json
 
-from time import sleep
 from typing import Any
-from pygame.math import Vector2
-from pygame.rect import Rect
+from pygame import Rect, Vector2
 
+from libs.course.course import draw_course
 
-with open("course.json", encoding="utf-8") as f_in:
-    COURSE = json.load(f_in)
 
 # pygame setup
 pygame.init()
@@ -21,8 +17,8 @@ DELTA_TIME = 0
 BALL_SPEED = 600
 BALL_SIZE = 10
 BALL_RADIUS = BALL_SIZE / 2
-PLAYER_START: Any = None
-PLAYER_POS: Any = None
+BALL_START_POS: Any = None
+BALL_POS: Any = None
 
 FONT = pygame.font.Font("Futura.ttc", 72)
 WINNER_TEXT = FONT.render("You Win!!!", True, (255, 255, 255))
@@ -31,21 +27,21 @@ WINNER_TEXT_RECT = WINNER_TEXT.get_rect(center=(1280 / 2, 720 / 2))
 
 def check_bounds() -> None:
     # left & right bounds of screen
-    if PLAYER_POS.x < BALL_SIZE:
-        PLAYER_POS.x = BALL_SIZE
-    elif PLAYER_POS.x > SCREEN.get_width() - BALL_SIZE:
-        PLAYER_POS.x = SCREEN.get_width() - BALL_SIZE
+    if BALL_POS.x < BALL_SIZE:
+        BALL_POS.x = BALL_SIZE
+    elif BALL_POS.x > SCREEN.get_width() - BALL_SIZE:
+        BALL_POS.x = SCREEN.get_width() - BALL_SIZE
     # top & bottom bounds of screen
-    if PLAYER_POS.y < BALL_SIZE:
-        PLAYER_POS.y = BALL_SIZE
-    elif PLAYER_POS.y > SCREEN.get_height() - BALL_SIZE:
-        PLAYER_POS.y = SCREEN.get_height() - BALL_SIZE
+    if BALL_POS.y < BALL_SIZE:
+        BALL_POS.y = BALL_SIZE
+    elif BALL_POS.y > SCREEN.get_height() - BALL_SIZE:
+        BALL_POS.y = SCREEN.get_height() - BALL_SIZE
 
 
 def detect_collision(course: list[Rect], hole: Rect, ball: Rect) -> None:
     if ball.collidelist(course) != -1:
-        PLAYER_POS.x = PLAYER_START.x
-        PLAYER_POS.y = PLAYER_START.y
+        BALL_POS.x = BALL_START_POS.x
+        BALL_POS.y = BALL_START_POS.y
     elif ball.colliderect(hole):
         SCREEN.blit(WINNER_TEXT, WINNER_TEXT_RECT)
 
@@ -53,54 +49,18 @@ def detect_collision(course: list[Rect], hole: Rect, ball: Rect) -> None:
 def move_ball() -> None:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_i] or keys[pygame.K_w]:
-        PLAYER_POS.y -= BALL_SPEED * DELTA_TIME
+        BALL_POS.y -= BALL_SPEED * DELTA_TIME
     if keys[pygame.K_k] or keys[pygame.K_s]:
-        PLAYER_POS.y += BALL_SPEED * DELTA_TIME
+        BALL_POS.y += BALL_SPEED * DELTA_TIME
     if keys[pygame.K_j] or keys[pygame.K_a]:
-        PLAYER_POS.x -= BALL_SPEED * DELTA_TIME
+        BALL_POS.x -= BALL_SPEED * DELTA_TIME
     if keys[pygame.K_l] or keys[pygame.K_d]:
-        PLAYER_POS.x += BALL_SPEED * DELTA_TIME
-
-
-def draw_course() -> tuple[list[Rect], Rect]:
-    global PLAYER_START, PLAYER_POS
-    box_size = 40
-    hole_size = 20
-    course: list[Rect] = []
-    hole: Rect = pygame.draw.circle(
-        SCREEN, "black", Vector2(hole_size, hole_size), hole_size
-    )
-
-    for y, row in enumerate(COURSE):
-        for x, value in enumerate(row):
-            if value == 1:
-                course.append(
-                    pygame.draw.rect(
-                        SCREEN,
-                        "brown",
-                        pygame.Rect(x * box_size, y * box_size, box_size, box_size),
-                        box_size,
-                    )
-                )
-            elif value == 2 and PLAYER_START is None:
-                PLAYER_START = Vector2(
-                    (x * box_size) + (BALL_SIZE * 2), y * box_size + BALL_SIZE
-                )
-                PLAYER_POS = Vector2(PLAYER_START)
-            elif value == 3:
-                hole_pos = Vector2(x * box_size + hole_size, y * box_size + hole_size)
-                hole = pygame.draw.circle(SCREEN, "black", hole_pos, hole_size)
-
-    return course, hole
-
-
-def draw_hole() -> Rect:
-    hole_pos = Vector2(SCREEN.get_width() - 120, SCREEN.get_height() - 120)
-    return pygame.draw.circle(SCREEN, "black", hole_pos, 20)
+        BALL_POS.x += BALL_SPEED * DELTA_TIME
 
 
 def main():
-    global RUNNING, DELTA_TIME
+    global RUNNING, DELTA_TIME, BALL_START_POS, BALL_POS
+    is_first_loop = True
 
     while RUNNING:
         # poll for events
@@ -111,10 +71,14 @@ def main():
 
         # fill the screen with a color to wipe away anything from last frame
         SCREEN.fill("green")
-        course, hole = draw_course()
-        ball = pygame.draw.circle(SCREEN, "white", PLAYER_POS, BALL_SIZE)
 
-        # draw_hole()
+        course, hole, ball_start_pos = draw_course(BALL_SIZE, SCREEN)
+        if is_first_loop:
+            BALL_START_POS = Vector2(ball_start_pos)
+            BALL_POS = Vector2(ball_start_pos)
+
+        ball = pygame.draw.circle(SCREEN, "white", BALL_POS, BALL_SIZE)
+
         move_ball()
         check_bounds()
         detect_collision(course, hole, ball)
@@ -125,6 +89,9 @@ def main():
         # limits FPS to 60
         # delta time in seconds since last frame, used for framerate-independent physics.
         DELTA_TIME = CLOCK.tick(60) / 1000
+
+        if is_first_loop:
+            is_first_loop = False
 
     pygame.quit()
 
