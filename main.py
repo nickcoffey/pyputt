6d9be4b1919fd1af07160562a927729d27dae1e0
Nick Coffey <nicholasjcoffey@gmail.com>
2025-03-04 16:07:35 -0600

    feat: added wall bounce physics

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ FONT = pygame.font.Font("Futura.ttc", 72)
 WINNER_TEXT = FONT.render("You Win!!!", True, (255, 255, 255), (0, 0, 0))
 WINNER_TEXT_RECT = WINNER_TEXT.get_rect(center=(1280 / 2, 720 / 2))
 
+COURSE_COLLISION_IDX = -1
+
 
 def main():
     global RUNNING, DELTA_TIME
@@ -28,9 +30,46 @@ def main():
     pause_handler = PauseHandler()
     ball = Ball()
 
+    def course_collision_check(ball: Ball):
+        global COURSE_COLLISION_IDX
+        coll = ball.rect.collidelist(course)
+        print(ball.rect.collidelistall(course))
+        COURSE_COLLISION_IDX = coll
+        return coll != -1
+
+    def course_collision_action(ball: Ball):
+        collided_wall = course[COURSE_COLLISION_IDX]
+        # print(collided_wall.right, collided_wall.bottom)
+        # print(
+        #     ball.rect.midtop, ball.rect.midbottom, ball.rect.midleft, ball.rect.midright
+        # )
+        if ball.y_speed < 0 and collided_wall.collidepoint(ball.rect.midtop):
+            print("HIT TOP")
+            ball.y_speed = ball.y_speed * -1
+            ball.position.y = collided_wall.bottom + (ball.rect.height / 2)
+        elif ball.y_speed > 0 and collided_wall.collidepoint(ball.rect.midbottom):
+            print("HIT BOTTOM")
+            ball.y_speed = ball.y_speed * -1
+            ball.position.y = collided_wall.top - (ball.rect.height / 2)
+        elif ball.x_speed < 0 and collided_wall.collidepoint(ball.rect.midleft):
+            print("HIT LEFT")
+            ball.x_speed = ball.x_speed * -1
+            ball.position.x = collided_wall.right + (ball.rect.width / 2)
+        elif ball.x_speed > 0 and collided_wall.collidepoint(ball.rect.midright):
+            print("HIT RIGHT")
+            ball.x_speed = ball.x_speed * -1
+            ball.position.x = collided_wall.left - (ball.rect.width / 2)
+        else:
+            print("HIT INTERSECTION")
+            pygame.quit()
+        #     # ball.position.x = collided_wall.x
+        #     ball.x_speed = ball.x_speed * -1
+        #     ball.position.x = collided_wall.x + ball.x_speed
+        # ball.move_to_start()
+
     course_collidable = Collidable(
-        collision_check=lambda ball: ball.rect.collidelist(course) != -1,
-        collision_action=lambda ball: ball.move_to_start(),
+        collision_check=course_collision_check,
+        collision_action=course_collision_action,
     )
     ball.add_collidable(course_collidable)
 
